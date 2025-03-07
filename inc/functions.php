3bb5fe7499c1b63c70d9672a3501b93acb9a702a
Nikolay Pekarskiy <dizer7@gmail.com>
2025-03-08 01:02:12 +0200

    fix prefix db

--- inc/functions.php
+++ inc/functions.php
@@ -196,27 +196,27 @@ function ip_woo_delete_attributes() {
     global $wpdb;
     
     // Видалення термінів атрибутів
-    $wpdb->query("DELETE FROM wp_terms WHERE term_id IN (SELECT term_id FROM wp_term_taxonomy WHERE taxonomy LIKE 'pa_%')");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}terms WHERE term_id IN (SELECT term_id FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy LIKE 'pa_%')");
     
     // Видалення метаданих термінів атрибутів
-    $wpdb->query("DELETE FROM wp_termmeta WHERE term_id IN (SELECT term_id FROM wp_term_taxonomy WHERE taxonomy LIKE 'pa_%')");
-    $wpdb->query("DELETE FROM wp_termmeta WHERE meta_key LIKE 'order_pa_%'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}termmeta WHERE term_id IN (SELECT term_id FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy LIKE 'pa_%')");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}termmeta WHERE meta_key LIKE 'order_pa_%'");
     
     // Видалення таксономій атрибутів
-    $wpdb->query("DELETE FROM wp_term_taxonomy WHERE taxonomy LIKE 'pa_%'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy LIKE 'pa_%'");
     
     // Видалення осиротілих зв'язків
-    $wpdb->query("DELETE FROM wp_term_relationships WHERE term_taxonomy_id NOT IN (SELECT term_taxonomy_id FROM wp_term_taxonomy)");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}term_relationships WHERE term_taxonomy_id NOT IN (SELECT term_taxonomy_id FROM {$wpdb->prefix}term_taxonomy)");
     
     // Видалення метаданих атрибутів з товарів
-    $wpdb->query("DELETE FROM wp_postmeta WHERE meta_key LIKE 'attribute_%'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}postmeta WHERE meta_key LIKE 'attribute_%'");
     
     // Видалення самих атрибутів з таблиці атрибутів WooCommerce
-    $wpdb->query("DELETE FROM wp_woocommerce_attribute_taxonomies");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}woocommerce_attribute_taxonomies");
     
     // Очищення кешу WooCommerce
-    $wpdb->query("DELETE FROM wp_options WHERE option_name LIKE '_transient_wc_%'");
-    $wpdb->query("DELETE FROM wp_options WHERE option_name LIKE '_transient_timeout_wc_%'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}options WHERE option_name LIKE '_transient_wc_%'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}options WHERE option_name LIKE '_transient_timeout_wc_%'");
     
     // Оновлення опції для сигналізації WooCommerce про необхідність оновлення
     update_option('woocommerce_attribute_lookup_regenerated', 0);
@@ -226,16 +226,16 @@ function ip_woo_delete_attributes() {
 function ip_woo_set_attributes_not_archives() {
     global $wpdb;
     
-    $wpdb->query("UPDATE wp_woocommerce_attribute_taxonomies SET attribute_public = '0' WHERE attribute_public = '1'");
+    $wpdb->query("UPDATE {$wpdb->prefix}woocommerce_attribute_taxonomies SET attribute_public = '0' WHERE attribute_public = '1'");
 }
 
 //SQL: Function to delete tags
 function ip_woo_delete_tags() {
     global $wpdb;
     
-    $wpdb->query("DELETE FROM wp_terms WHERE term_id IN (SELECT term_id FROM wp_term_taxonomy WHERE taxonomy = 'product_tag')");
-    $wpdb->query("DELETE FROM wp_term_taxonomy WHERE taxonomy = 'product_tag'");
-    $wpdb->query("DELETE FROM wp_term_relationships WHERE term_taxonomy_id NOT IN (SELECT term_taxonomy_id FROM wp_term_taxonomy)");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}terms WHERE term_id IN (SELECT term_id FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy = 'product_tag')");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy = 'product_tag'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}term_relationships WHERE term_taxonomy_id NOT IN (SELECT term_taxonomy_id FROM {$wpdb->prefix}term_taxonomy)");
 }
 
 //SQL: Function to delete Products
@@ -244,21 +244,21 @@ function ip_woo_delete_products() {
 
     // Видалення зв'язків термінів із товарами
     $wpdb->query("DELETE relations.* 
-                  FROM wp_term_relationships AS relations
-                  INNER JOIN wp_term_taxonomy AS taxes ON relations.term_taxonomy_id = taxes.term_taxonomy_id
-                  WHERE object_id IN (SELECT ID FROM wp_posts WHERE post_type = 'product')");
+                  FROM {$wpdb->prefix}term_relationships AS relations
+                  INNER JOIN {$wpdb->prefix}term_taxonomy AS taxes ON relations.term_taxonomy_id = taxes.term_taxonomy_id
+                  WHERE object_id IN (SELECT ID FROM {$wpdb->prefix}posts WHERE post_type = 'product')");
 
     // Видалення метаданих товарів
-    $wpdb->query("DELETE FROM wp_postmeta WHERE post_id IN (SELECT ID FROM wp_posts WHERE post_type = 'product')");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}postmeta WHERE post_id IN (SELECT ID FROM {$wpdb->prefix}posts WHERE post_type = 'product')");
 
     // Видалення самих товарів
-    $wpdb->query("DELETE FROM wp_posts WHERE post_type = 'product'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}posts WHERE post_type = 'product'");
 
     // Видалення осиротілих метаданих
-    $wpdb->query("DELETE pm FROM wp_postmeta pm LEFT JOIN wp_posts wp ON wp.ID = pm.post_id WHERE wp.ID IS NULL");
+    $wpdb->query("DELETE pm FROM {$wpdb->prefix}postmeta pm LEFT JOIN {$wpdb->prefix}posts wp ON wp.ID = pm.post_id WHERE wp.ID IS NULL");
 
     // Видалення WooCommerce сесій
-    $wpdb->query("DELETE FROM wp_woocommerce_sessions");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}woocommerce_sessions");
 }
 
 //SQL: Function to delete Product Categories
@@ -266,19 +266,19 @@ function ip_woo_delete_product_categories() {
     global $wpdb;
 
     // Видалення метаданих термінів для категорій товарів
-    $wpdb->query("DELETE FROM wp_termmeta WHERE term_id IN (
-                  SELECT term_id FROM wp_term_taxonomy WHERE taxonomy IN ('product_cat', 'product_type', 'product_visibility'))");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}termmeta WHERE term_id IN (
+                  SELECT term_id FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy IN ('product_cat', 'product_type', 'product_visibility'))");
 
     // Видалення самих термінів (категорій товарів)
-    $wpdb->query("DELETE FROM wp_terms WHERE term_id IN (
-                  SELECT term_id FROM wp_term_taxonomy WHERE taxonomy IN ('product_cat', 'product_type', 'product_visibility'))");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}terms WHERE term_id IN (
+                  SELECT term_id FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy IN ('product_cat', 'product_type', 'product_visibility'))");
 
     // Видалення таксономій категорій товарів
-    $wpdb->query("DELETE FROM wp_term_taxonomy WHERE taxonomy IN ('product_cat', 'product_type', 'product_visibility')");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}term_taxonomy WHERE taxonomy IN ('product_cat', 'product_type', 'product_visibility')");
 
     // Видалення осиротілих метаданих термінів
-    $wpdb->query("DELETE meta FROM wp_termmeta meta 
-                  LEFT JOIN wp_terms terms ON terms.term_id = meta.term_id 
+    $wpdb->query("DELETE meta FROM {$wpdb->prefix}termmeta meta 
+                  LEFT JOIN {$wpdb->prefix}terms terms ON terms.term_id = meta.term_id 
                   WHERE terms.term_id IS NULL");
 }
 
@@ -308,10 +308,10 @@ function ip_woo_delete_orders() {
         $wpdb->query("DELETE FROM {$wpdb->prefix}posts WHERE post_type = 'shop_order'");
     } else {
         // Для старої структури даних видаляємо замовлення і мета-дані для старих таблиць
-        $wpdb->query("DELETE FROM {$wpdb->prefix}wp_wc_orders_meta");
-        $wpdb->query("DELETE FROM {$wpdb->prefix}wp_wc_orders");
-        $wpdb->query("DELETE FROM {$wpdb->prefix}wp_wc_order_addresses");
-        $wpdb->query("DELETE FROM {$wpdb->prefix}wp_wc_order_operational_data");
+        $wpdb->query("DELETE FROM {$wpdb->prefix}wc_orders_meta");
+        $wpdb->query("DELETE FROM {$wpdb->prefix}wc_orders");
+        $wpdb->query("DELETE FROM {$wpdb->prefix}wc_order_addresses");
+        $wpdb->query("DELETE FROM {$wpdb->prefix}wc_order_operational_data");
     }
 
     // Видаляємо коментарі та мета-дані для замовлень
@@ -334,16 +334,16 @@ function ip_woo_delete_orders() {
 function ip_woo_delete_products_trashed() {
     global $wpdb;
 
-    $wpdb->query("DELETE FROM wp_postmeta WHERE post_id IN (SELECT ID FROM wp_posts WHERE post_type = 'product' AND post_status = 'trash')");
-    $wpdb->query("DELETE FROM wp_posts WHERE post_type = 'product' AND post_status = 'trash'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}postmeta WHERE post_id IN (SELECT ID FROM {$wpdb->prefix}posts WHERE post_type = 'product' AND post_status = 'trash')");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}posts WHERE post_type = 'product' AND post_status = 'trash'");
 }
 
 //SQL: Function to delete all coupons
 function ip_woo_delete_coupons() {
     global $wpdb;
 
-    $wpdb->query("DELETE FROM wp_postmeta WHERE post_id IN (SELECT ID FROM wp_posts WHERE post_type = 'shop_coupon')");
-    $wpdb->query("DELETE FROM wp_posts WHERE post_type = 'shop_coupon'");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}postmeta WHERE post_id IN (SELECT ID FROM {$wpdb->prefix}posts WHERE post_type = 'shop_coupon')");
+    $wpdb->query("DELETE FROM {$wpdb->prefix}posts WHERE post_type = 'shop_coupon'");
 }
 
 //SQL: Function to delete all order notes
